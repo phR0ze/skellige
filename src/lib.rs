8afa6e4603113b8ca876ef84feec66cd49168b2d
phR0ze <prc451@gmail.com>
2020-12-27 11:26:48 -0700

    Initial project structure

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// Unit tests
+// -------------------------------------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    // #[test]
+    // fn test_color_enabled() {
+    //     assert!(Color::enabled() || !Color::enabled());
+    // }
+}
